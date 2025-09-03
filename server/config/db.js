d6d30f910a97a1ab232081152ea9bcceffd50a88
Aldo Citalan <aldocitalan81@gmail.com>
2025-09-03 13:25:55 -0600

    Refactor database configuration to use environment variables for improved security and flexibility

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'jesus', // Cambia por tu usuario de MySQL
-    password: '1234', // Cambia por tu contraseña de MySQL
-    database: 'control_acceso',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
