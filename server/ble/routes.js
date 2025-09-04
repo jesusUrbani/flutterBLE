ef6e48e660619e38265afaaee424defed8f15758
Aldo Citalan <aldocitalan81@gmail.com>
2025-09-03 18:08:07 -0600

    Change BLE registration endpoint from POST to PUT and update validation middleware

--- server/ble/routes.js
+++ server/ble/routes.js
@@ -3,7 +3,7 @@ const { body } = require('express-validator');
 const validatorMiddleware = require('../common/middlewares/validator');
 const { completeRegistration } = require('./controller');
 
-router.post('/', [
+router.put('/', [
     body('id_usuario').trim().notEmpty().withMessage('id_usuario es requerido'),
     validatorMiddleware,
     completeRegistration
